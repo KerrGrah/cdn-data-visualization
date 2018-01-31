6c43370475ac0045d24b30e818ca7375fa98444d
Graham <grey.ham.acre@gmail.com>
2018-01-31 13:26:08 +0300

    changes to api error handling

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -35,7 +35,7 @@ export function logIn(userId, password) {
 
 export function logOut(sessionId) {
   return function(dispatch) {
-
+    console.log('logging out is automatic for dev purposes hence the 403');
     dispatch({type: "LOGGING_OUT_USER"});
     fetch(url + '/logout', {
       method: 'POST',
@@ -45,101 +45,103 @@ export function logOut(sessionId) {
       },
       body: `session_token=${sessionId}`
     }).then((res) => {
-      dispatch({type: "USER_LOG_OUT_SUCCESS"});
+      if (res.ok) {
+        dispatch({type: "USER_LOG_OUT_SUCCESS"});
+      }
     }).catch(error => {
-      //console.log('this error is harmless - logout is automatic for dev purposes');
-      //console.error('Error:', error)
-      dispatch({type: "LOG_OUT_REJECTED", payload: error})
-    })
-  }
+    dispatch({type: "LOG_OUT_REJECTED", payload: error})
+  })
+}
 }
 
 function getClientData(dispatch, sessionToken) {
 
-  dispatch({type: "FETCHING_CLIENT_DATA"});
-  const aggregateParams = ['', 'sum', 'average', 'max', 'min'];
-  let promises = [];
-  aggregateParams.forEach(param => {
-    promises.push(getBandwidthData(sessionToken, param))
-  })
-  aggregateParams.forEach(param => {
-    promises.push(getAudienceData(sessionToken, param))
-  })
-  Promise.all(promises).then((data) => {
+dispatch({type: "FETCHING_CLIENT_DATA"});
+const aggregateParams = ['', 'sum', 'average', 'max', 'min'];
+let promises = [];
+aggregateParams.forEach(param => {
+  promises.push(getBandwidthData(sessionToken, param))
+})
+aggregateParams.forEach(param => {
+  promises.push(getAudienceData(sessionToken, param))
+})
+Promise.all(promises).then((data) => {
 
-    //console.log(data);
-    let p2p = []
-    data[0].p2p.forEach(cell => {
-      p2p.push([
-        cell[0], cell[1] / 1000 / 1000 / 1000
-      ]);
-    })
-    let cdn = []
-    data[0].cdn.forEach(cell => {
-      cdn.push([
-        cell[0], cell[1] / 1000 / 1000 / 1000
-      ]);
-    })
-    const cdnp2p = {
-      cdn: cdn,
-      p2p: p2p
-    }
-    const result = {
-      bandwidth: {
-        data: cdnp2p,
-        sum: data[1],
-        average: data[2],
-        max: data[3],
-        min: data[4]
-      },
-      audience: {
-        data: data[5],
-        sum: data[6],
-        average: data[7],
-        max: data[8],
-        min: data[9]
-      }
-    }
-  //  console.log("RESULT ", result);
-    dispatch({type: "FETCHING_CLIENT_DATA_FULFILLED", payload: result});
-  }).catch(err => {
-    console.log(err.message);
-    dispatch({type: "FETCHING_BANDWIDTH_DATA_REJECTED", payload: err});
+  //console.log(data);
+  let p2p = []
+  data[0].p2p.forEach(cell => {
+    p2p.push([
+      cell[0], cell[1] / 1000 / 1000 / 1000
+    ]);
   })
+  let cdn = []
+  data[0].cdn.forEach(cell => {
+    cdn.push([
+      cell[0], cell[1] / 1000 / 1000 / 1000
+    ]);
+  })
+  const cdnp2p = {
+    cdn: cdn,
+    p2p: p2p
+  }
+  const result = {
+    bandwidth: {
+      data: cdnp2p,
+      sum: data[1],
+      average: data[2],
+      max: data[3],
+      min: data[4]
+    },
+    audience: {
+      data: data[5],
+      sum: data[6],
+      average: data[7],
+      max: data[8],
+      min: data[9]
+    }
+  }
+  //  console.log("RESULT ", result);
+  dispatch({type: "FETCHING_CLIENT_DATA_FULFILLED", payload: result});
+}).catch(err => {
+  console.log(err.message);
+  dispatch({type: "FETCHING_BANDWIDTH_DATA_REJECTED", payload: err});
+})
 }
 
 function getBandwidthData(sessionId, param, from, to) {
-  from = from || fromTimestamp;
-  to = to || nowTimestamp;
-  const aggregate = param
-    ? '&aggregate=' + param
-    : '';
-  return fetch(url + '/bandwidth', {
-    method: 'POST',
-    "crossDomain": true,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: `session_token=${sessionId}&from=${from}&to=${to}${aggregate}`
-  }).then(res => res.json()).catch(error => {
-    console.error('Error:', error)
-  })
+from = from || fromTimestamp;
+to = to || nowTimestamp;
+const aggregate = param
+  ? '&aggregate=' + param
+  : '';
+return fetch(url + '/bandwidth', {
+  method: 'POST',
+  "crossDomain": true,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: `session_token=${sessionId}&from=${from}&to=${to}${aggregate}`
+}).then(res => handleErrors(res))
 }
 
 function getAudienceData(sessionId, param, from, to) {
-  from = from || fromTimestamp;
-  to = to || nowTimestamp;
-  const aggregate = param
-    ? '&aggregate=' + param
-    : '';
-  return fetch(url + '/audience', {
-    method: 'POST',
-    "crossDomain": true,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: `session_token=${sessionId}&from=${from}&to=${to}${aggregate}`
-  }).then(res => res.json()).catch(error => {
-    console.error('Error:', error)
-  })
+from = from || fromTimestamp;
+to = to || nowTimestamp;
+const aggregate = param
+  ? '&aggregate=' + param
+  : '';
+return fetch(url + '/audience', {
+  method: 'POST',
+  "crossDomain": true,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  body: `session_token=${sessionId}&from=${from}&to=${to}${aggregate}`
+}).then(res => handleErrors(res))
+}
+
+function handleErrors(response) {
+    if (response.ok)  return response.json();
+    else if (response.status === 404 ) console.log('no data available');
+    else throw Error(response.statusText);
 }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
-export const url = 'https://cdn-data-vis-back.herokuapp.com';
+export const url = 'http://localhost:3000'//'https://cdn-data-vis-back.herokuapp.com';
 
 //'http://localhost:3000'
